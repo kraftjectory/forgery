e839e978cfd068c6cda7e4e5853ba04e1df6f3e7
Aleksei Magusev <lexmag@me.com>
2020-11-10 00:54:11 +0100

    Improve testing setup

--- test/forgery_test.exs
+++ test/forgery_test.exs
@@ -1,52 +1,49 @@
 defmodule ForgeryTest do
   use ExUnit.Case, async: true
 
-  doctest Forgery
-
   defmodule User do
-    defstruct [:id, :username, :password]
+    defstruct [:id, :name, :password]
   end
 
-  defmodule DummyFactory do
+  defmodule MyFactory do
     use Forgery
 
     def make(:user, fields) do
       id = make_unique_integer()
-
-      make_foo = fn -> raise("asdfasdf") end
+      make_foo = fn -> raise("unexpected") end
 
       fields
       |> put_new_field(:id, id)
-      |> put_new_field(:username, "user#{id}")
-      |> put_new_field(:username, make_foo.())
+      |> put_new_field(:name, "user##{id}")
+      |> put_new_field(:name, make_foo.())
       |> create_struct(User)
     end
   end
 
+  doctest Forgery
+
   test "make/1 and make/2" do
     assert %User{
              id: id,
-             username: username
-           } = DummyFactory.make(:user)
+             name: name
+           } = MyFactory.make(:user)
 
     assert is_integer(id)
     assert id > 0
-    assert username == "user#{id}"
+    assert name == "user##{id}"
 
     assert %User{
              id: 100,
-             username: username
-           } = DummyFactory.make(:user, id: 100)
+             name: "user" <> _
+           } = MyFactory.make(:user, id: 100)
 
-    assert "user" <> _ = username
-
-    assert %User{username: "john"} = DummyFactory.make(:user, username: "john")
+    assert %User{name: "John"} = MyFactory.make(:user, name: "John")
   end
 
   test "make_many/4" do
-    assert [user1, user2] = DummyFactory.make_many(:user, 2, %{password: "123456"})
+    assert [user1, user2] = MyFactory.make_many(:user, 2, %{password: "123456"})
     assert user1.id != user2.id
-    assert user1.username != user2.username
+    assert user1.name != user2.name
 
     assert user1.password == "123456"
     assert user2.password == "123456"
